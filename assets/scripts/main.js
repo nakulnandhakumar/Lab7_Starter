26b5f87a1ebb63c41e581097b6e64ba9719f989e
nakulnandhakumar <nakul@nandhakumar.net>
2023-06-11 10:04:17 -0700

    changes made to fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,6 +53,24 @@ function initializeServiceWorker() {
   //            log that it was successful.
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+  if ('serviceWorker' in navigator) {
+    window.addEventListener("load", async function() {
+      try {
+        const registration = await navigator.serviceWorker.register("sw.js", {
+          scope: "",
+        });
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
--- sw.js
+++ sw.js
@@ -9,7 +9,13 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+      'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+      'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+      'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+      'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+      'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+      'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',]);
     })
   );
 });
@@ -20,7 +26,7 @@ self.addEventListener('activate', function (event) {
 });
 
 // Intercept fetch requests and cache them
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', async function (event) {
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
@@ -37,4 +43,17 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+        // Add the network response to the cache for future visits.
+        // Note: we need to make a copy of the response to save it in
+        // the cache and use the original as the request response.
+        cache.put(event.request, fetchedResponse.clone());
+
+        // Return the network response
+        return fetchedResponse;
+      });
+    });
+  }));
 });
